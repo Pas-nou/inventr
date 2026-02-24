0dadfed1e0b784db6d6eca8668fafba4134ede7c
Pas_nou <pascal.renou@live.fr>
2026-02-24 15:13:25 +0100

    feat: implement AuthController with login and register routes

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  login(@Body() signInDto: LoginDto) {
+    return this.authService.login(signInDto.email, signInDto.password);
+  }
+  @Post('register')
+  register(@Body() signUpDto: RegisterDto) {
+    return this.authService.register(signUpDto.email, signUpDto.password);
+  }
+}
