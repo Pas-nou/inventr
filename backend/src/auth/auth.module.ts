1f49f02840b9ba281b2e3b96703369dd4892fb69
Pas_nou <pascal.renou@live.fr>
2026-02-24 16:46:42 +0100

    feat: fix JWT auth and implement AssetController

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -18,7 +18,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       useFactory: (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
         signOptions: {
-          expiresIn: (configService.get<string>('JWT_EXPIRATION') || '1h') as StringValue,
+          expiresIn: (configService.get<string>('JWT_EXPIRATION') ||
+            '1h') as StringValue,
         },
       }),
     }),
