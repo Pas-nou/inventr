b7a3bfddc47743f2aca21573d2b77e4aca1e2c05
Pas_nou <pascal.renou@live.fr>
2026-03-06 16:59:30 +0100

    feat: user data in login response

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -53,6 +53,12 @@ export class AuthService {
     const payload = { email: user.email, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
+      user: {
+        id: user.id,
+        email: user.email,
+        first_name: user.first_name,
+        last_name: user.last_name,
+      },
     };
   }
 }
