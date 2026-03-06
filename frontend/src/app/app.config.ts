a8a070b20a1bd44b5b7952eaecfacb406cf0a840
Pas_nou <pascal.renou@live.fr>
2026-03-06 16:03:35 +0100

    feat: add home page header, stats cards and warranty alert card

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,6 +4,11 @@ import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@a
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import { LOCALE_ID } from '@angular/core';
+
+registerLocaleData(localeFr);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,9 +19,7 @@ export const appConfig: ApplicationConfig = {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
     }),
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }, provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: LOCALE_ID, useValue: 'fr' },
   ],
 };
--- frontend/src/app/features/assets/assets.ts
+++ frontend/src/app/features/assets/assets.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-
+import { CurrencyPipe } from '@angular/common';
+import { LucideAngularModule, TriangleAlert } from 'lucide-angular';
 @Component({
   selector: 'app-assets',
-  imports: [],
+  imports: [CurrencyPipe, LucideAngularModule],
   templateUrl: './assets.html',
   styleUrl: './assets.css',
 })
 export class AssetsComponent {
-
+  triangleAlert = TriangleAlert;
+  // Provisional data
+  firstName = 'John';
+  assetsCount = 24;
+  totalValue = 12450;
+  warrantyAlerts = ['MacBook Pro', 'iPhone 15 Pro'];
 }
