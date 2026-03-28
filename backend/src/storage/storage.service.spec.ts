434e33c51c06ec6a273ad20374a48e039588c30b
Pas_nou <pascal.renou@live.fr>
2026-03-28 18:40:51 +0100

    fix: add ConfigService mock in StorageService spec

--- backend/src/storage/storage.service.spec.ts
+++ backend/src/storage/storage.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StorageService } from './storage.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('StorageService', () => {
   let service: StorageService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StorageService],
+      providers: [
+        StorageService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn().mockReturnValue('mock_value'),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<StorageService>(StorageService);
